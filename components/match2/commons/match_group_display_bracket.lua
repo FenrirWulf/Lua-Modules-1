Liquipedia/Lua-Modules

Add bracketData.lowerEdges

components/match2/commons/match_group_display_bracket.lua
@@ -158,10 +158,7 @@ function BracketDisplay.computeBracketLayout(bracket, config, headerRowsByMatchI
 		local match = bracket.matchesById[matchId]
 
 		-- Recurse on lower round matches
-		local lowerLayouts = Array.map(
-			match.bracketData.lowerMatches,
-			function(lowerMatch) return computeNodeLayout(lowerMatch.matchId) end
-		)
+		local lowerLayouts = Array.map(match.bracketData.lowerMatchIds, computeNodeLayout)
 
 		-- Compute partial sums of heights of lower round matches
 		local heightSums = Math.partialSums(
@@ -217,7 +214,7 @@ function BracketDisplay.alignMatchWithLowerNodes(match, lowerLayouts, heightSums
 	-- match advancing an opponent that is placed near the middle of this match.
 	local showSingleStraightLine = false
 	if #lowerLayouts == 1 then
-		local opponentIx = match.bracketData.lowerMatches[1].opponentIx
+		local opponentIx = match.bracketData.lowerEdges[1].opponentIx
 		if #match.opponents % 2 == 0 then
 			showSingleStraightLine = opponentIx == #match.opponents / 2
 				or opponentIx == #match.opponents / 2 + 1
@@ -230,7 +227,7 @@ function BracketDisplay.alignMatchWithLowerNodes(match, lowerLayouts, heightSums
 	if showSingleStraightLine then
 		-- Single straight line: Align the connecting line with the middle
 		-- of the opponent it connects into.
-		local opponentIx = match.bracketData.lowerMatches[1].opponentIx
+		local opponentIx = match.bracketData.lowerEdges[1].opponentIx
 		return lowerLayouts[1].mid
 			- ((opponentIx - 1) + 0.5) * opponentHeight
 
@@ -269,7 +266,7 @@ function BracketDisplay.computeHeaderRows(bracket, config)
 		local upperBracketData = bracket.upperMatchIds[matchId]
 			and bracket.bracketDatasById[bracket.upperMatchIds[matchId]]
 		local isFirstChild = upperBracketData
-			and matchId == upperBracketData.lowerMatches[1].matchId
+			and matchId == upperBracketData.lowerMatchIds[1]
 		local showHeader = bracketData.header and not isFirstChild
 		if showHeader then
 			headerRows[matchId] = {}
@@ -420,11 +417,11 @@ function BracketDisplay.NodeBody(props)
 
 	-- Matches from lower rounds
 	local lowerNode
-	if 0 < #match.bracketData.lowerMatches then
+	if 0 < #match.bracketData.lowerMatchIds then
 		lowerNode = html.create('div'):addClass('brkts-round-lower')
 			:css('margin-top', layout.lowerNodeMarginTop .. 'px')
-		for _, lowerMatch in ipairs(match.bracketData.lowerMatches) do
-			local childProps = Table.merge(props, {matchId = lowerMatch.matchId})
+		for _, lowerMatchId in ipairs(match.bracketData.lowerMatchIds) do
+			local childProps = Table.merge(props, {matchId = lowerMatchId})
 			lowerNode
 				:node(BracketDisplay.NodeHeader(childProps))
 				:node(BracketDisplay.NodeBody(childProps))
@@ -609,11 +606,11 @@ function BracketDisplay.NodeLowerConnectors(props)
 	local match = props.matchesById[props.matchId]
 	local layout = props.layoutsByMatchId[props.matchId]
 	local config = props.config
-	local lowerMatches = match.bracketData.lowerMatches
+	local bracketData = match.bracketData
 
 	local lowerLayouts = Array.map(
-		lowerMatches,
-		function(lowerMatch) return props.layoutsByMatchId[lowerMatch.matchId] end
+		bracketData.lowerMatchIds,
+		function(lowerMatchId) return props.layoutsByMatchId[lowerMatchId] end
 	)
 
 	-- Compute partial sums of heights of lower round matches
@@ -624,26 +621,32 @@ function BracketDisplay.NodeLowerConnectors(props)
 	-- Compute joints of connectors
 	local jointIxs = {}
 	local jointIxAbove = 0
-	for ix = math.ceil(#lowerMatches / 2), 1, -1 do
-		jointIxAbove = jointIxAbove + 1
-		jointIxs[lowerMatches[ix].opponentIx] = jointIxAbove
+	for ix = math.ceil(#bracketData.lowerEdges / 2), 1, -1 do
+		local lowerEdge = bracketData.lowerEdges[ix]
+		if not jointIxs[lowerEdge.opponentIx] then
+			jointIxAbove = jointIxAbove + 1
+			jointIxs[lowerEdge.opponentIx] = jointIxAbove
+		end
 	end
 	local jointIxBelow = 0
 	-- middle lower match is repeated if odd
-	for ix = math.floor(#lowerMatches / 2) + 1, #lowerMatches, 1 do
-		jointIxBelow = jointIxBelow + 1
-		jointIxs[lowerMatches[ix].opponentIx] = jointIxBelow
+	for ix = math.floor(#bracketData.lowerEdges / 2) + 1, #bracketData.lowerEdges, 1 do
+		local lowerEdge = bracketData.lowerEdges[ix]
+		if not jointIxs[lowerEdge.opponentIx] then
+			jointIxBelow = jointIxBelow + 1
+			jointIxs[lowerEdge.opponentIx] = jointIxBelow
+		end
 	end
 	local jointCount = math.max(jointIxAbove, jointIxBelow)
 
 	local lowerConnectorsNode = mw.html.create('div'):addClass('brkts-round-lower-connectors')
 
 	-- Draw connectors between lower round matches and this match
-	for ix, x in ipairs(lowerMatches) do
-		local lowerLayout = lowerLayouts[ix]
-		local leftTop = layout.lowerNodeMarginTop + heightSums[ix] + lowerLayout.mid
-		local rightTop = layout.matchMarginTop + ((x.opponentIx - 1) + 0.5) * config.opponentHeight
-		local jointLeft = (config.roundHorizontalMargin - 2) * jointIxs[x.opponentIx] / (jointCount + 1)
+	for _, lowerEdge in ipairs(bracketData.lowerEdges) do
+		local lowerLayout = lowerLayouts[lowerEdge.lowerMatchIx]
+		local leftTop = layout.lowerNodeMarginTop + heightSums[lowerEdge.lowerMatchIx] + lowerLayout.mid
+		local rightTop = layout.matchMarginTop + ((lowerEdge.opponentIx - 1) + 0.5) * config.opponentHeight
+		local jointLeft = (config.roundHorizontalMargin - 2) * jointIxs[lowerEdge.opponentIx] / (jointCount + 1)
 
 		local segment1Node = html.create('div'):addClass('brkts-line')
 			:css('height', config.lineWidth .. 'px')

components/match2/commons/match_group_util.lua
@@ -29,8 +29,8 @@ Display related functions go in Module:MatchGroup/Display/Helper.
 ]]
 local MatchGroupUtil = {types = {}}
 
-MatchGroupUtil.types.LowerMatch = TypeUtil.struct({
-	matchId = 'string',
+MatchGroupUtil.types.LowerEdge = TypeUtil.struct({
+	lowerMatchIx = 'number',
 	opponentIx = 'number',
 })
 MatchGroupUtil.types.AdvanceBg = TypeUtil.literalUnion('up', 'stayup', 'stay', 'staydown', 'down')
@@ -44,7 +44,8 @@ MatchGroupUtil.types.BracketBracketData = TypeUtil.struct({
 	bracketResetMatchId = 'string?',
 	bracketSection = 'string',
 	header = 'string?',
-	lowerMatches = TypeUtil.array(MatchGroupUtil.types.LowerMatch),
+	lowerMatchIds = TypeUtil.array('string'),
+	lowerEdges = TypeUtil.array(MatchGroupUtil.types.LowerEdge),
 	qualLose = 'boolean?',
 	qualLoseLiteral = 'string?',
 	qualSkip = 'number?',
@@ -273,18 +274,21 @@ end
 
 function MatchGroupUtil.bracketDataFromRecord(data, opponentCount)
 	if data.type == 'bracket' then
-		local lowerMatches = {}
-		local midIx = math.ceil(opponentCount / 2)
+		local lowerEdges = {}
+		local lowerMatchIds = {}
+		local midIx = math.floor(opponentCount / 2)
 		if nilIfEmpty(data.toupper) then
-			table.insert(lowerMatches, {
-				matchId = data.toupper,
+			table.insert(lowerMatchIds, data.toupper)
+			table.insert(lowerEdges, {
 				opponentIx = midIx,
+				lowerMatchIx = #lowerMatchIds,
 			})
 		end
 		if nilIfEmpty(data.tolower) then
-			table.insert(lowerMatches, {
-				matchId = data.tolower,
+			table.insert(lowerMatchIds, data.tolower)
+			table.insert(lowerEdges, {
 				opponentIx = math.min(midIx + 1, opponentCount),
+				lowerMatchIx = #lowerMatchIds,
 			})
 		end
 
@@ -307,7 +311,8 @@ function MatchGroupUtil.bracketDataFromRecord(data, opponentCount)
 			bracketResetMatchId = nilIfEmpty(data.bracketreset),
 			bracketSection = data.bracketsection,
 			header = nilIfEmpty(data.header),
-			lowerMatches = lowerMatches,
+			lowerEdges = lowerEdges,
+			lowerMatchIds = lowerMatchIds,
 			qualLose = advanceSpots[2] and advanceSpots[2].type == 'qualify',
 			qualLoseLiteral = nilIfEmpty(data.qualloseLiteral),
 			qualSkip = tonumber(data.qualskip) or data.qualskip == 'true' and 1 or 0,
@@ -392,8 +397,8 @@ function MatchGroupUtil.computeUpperMatchIds(matchesById)
 	local upperMatchIds = {}
 	for matchId, match in pairs(matchesById) do
 		if match.bracketData.type == 'bracket' then
-			for _, lowerMatch in ipairs(match.bracketData.lowerMatches) do
-				upperMatchIds[lowerMatch.matchId] = matchId
+			for _, lowerMatchId in ipairs(match.bracketData.lowerMatchIds) do
+				upperMatchIds[lowerMatchId] = matchId
 			end
 		end
 	end
@@ -418,10 +423,7 @@ end
 function MatchGroupUtil.dfsFrom(bracketDatasById, start)
 	return TreeUtil.dfs(
 		function(matchId)
-			return Array.map(
-				bracketDatasById[matchId].lowerMatches,
-				function(lowerMatch) return lowerMatch.matchId end
-			)
+			return bracketDatasById[matchId].lowerMatchIds
 		end,
 		start
 	)
@@ -434,8 +436,8 @@ function MatchGroupUtil.computeDepthsFrom(bracketDatasById, startMatchId)
 		local bracketData = bracketDatasById[matchId]
 		depths[matchId] = depth
 		maxDepth = math.max(maxDepth, depth + bracketData.skipRound)
-		for _, lowerMatch in ipairs(bracketData.lowerMatches) do
-			visit(lowerMatch.matchId, depth + 1 + bracketData.skipRound)
+		for _, lowerMatchId in ipairs(bracketData.lowerMatchIds) do
+			visit(lowerMatchId, depth + 1 + bracketData.skipRound)
 		end
 	end
 	visit(startMatchId, 0)
@@ -497,8 +499,8 @@ function MatchGroupUtil.populateAdvanceSpots(bracket)
 	local firstBracketData = bracket.bracketDatasById[bracket.rootMatchIds[1]]
 	local thirdPlaceMatchId = firstBracketData.thirdPlaceMatchId
 	if thirdPlaceMatchId and bracket.matchesById[thirdPlaceMatchId] then
-		for _, lowerMatch in ipairs(firstBracketData.lowerMatches) do
-			local bracketData = bracket.bracketDatasById[lowerMatch.matchId]
+		for _, lowerMatchId in ipairs(firstBracketData.lowerMatchIds) do
+			local bracketData = bracket.bracketDatasById[lowerMatchId]
 			bracketData.advanceSpots[2] = bracketData.advanceSpots[2]
 				or {bg = 'stayup', type = 'advance', matchId = thirdPlaceMatchId}
 		end

FenrirWulf/Lua-Modules-1

components/match2/commons/match_group_display_bracket.lua
@@ -158,10 +158,7 @@ function BracketDisplay.computeBracketLayout(bracket, config, headerRowsByMatchI
 		local match = bracket.matchesById[matchId]
 
 		-- Recurse on lower round matches
-		local lowerLayouts = Array.map(
-			match.bracketData.lowerMatches,
-			function(lowerMatch) return computeNodeLayout(lowerMatch.matchId) end
-		)
+		local lowerLayouts = Array.map(match.bracketData.lowerMatchIds, computeNodeLayout)
 
 		-- Compute partial sums of heights of lower round matches
 		local heightSums = MathUtil.partialSums(
@@ -217,7 +214,7 @@ function BracketDisplay.alignMatchWithLowerNodes(match, lowerLayouts, heightSums
 	-- match advancing an opponent that is placed near the middle of this match.
 	local showSingleStraightLine = false
 	if #lowerLayouts == 1 then
-		local opponentIx = match.bracketData.lowerMatches[1].opponentIx
+		local opponentIx = match.bracketData.lowerEdges[1].opponentIndex
 		if #match.opponents % 2 == 0 then
 			showSingleStraightLine = opponentIx == #match.opponents / 2
 				or opponentIx == #match.opponents / 2 + 1
@@ -230,7 +227,7 @@ function BracketDisplay.alignMatchWithLowerNodes(match, lowerLayouts, heightSums
 	if showSingleStraightLine then
 		-- Single straight line: Align the connecting line with the middle
 		-- of the opponent it connects into.
-		local opponentIx = match.bracketData.lowerMatches[1].opponentIx
+		local opponentIx = match.bracketData.lowerEdges[1].opponentIndex
 		return lowerLayouts[1].mid
 			- ((opponentIx - 1) + 0.5) * opponentHeight
 
@@ -269,7 +266,7 @@ function BracketDisplay.computeHeaderRows(bracket, config)
 		local upperBracketData = bracket.upperMatchIds[matchId]
 			and bracket.bracketDatasById[bracket.upperMatchIds[matchId]]
 		local isFirstChild = upperBracketData
-			and matchId == upperBracketData.lowerMatches[1].matchId
+			and matchId == upperBracketData.lowerMatchIds[1]
 		local showHeader = bracketData.header and not isFirstChild
 		if showHeader then
 			headerRows[matchId] = {}
@@ -420,11 +417,11 @@ function BracketDisplay.NodeBody(props)
 
 	-- Matches from lower rounds
 	local lowerNode
-	if 0 < #match.bracketData.lowerMatches then
+	if 0 < #match.bracketData.lowerMatchIds then
 		lowerNode = html.create('div'):addClass('brkts-round-lower')
 			:css('margin-top', layout.lowerNodeMarginTop .. 'px')
-		for _, lowerMatch in ipairs(match.bracketData.lowerMatches) do
-			local childProps = Table.merge(props, {matchId = lowerMatch.matchId})
+		for _, lowerMatchId in ipairs(match.bracketData.lowerMatchIds) do
+			local childProps = Table.merge(props, {matchId = lowerMatchId})
 			lowerNode
 				:node(BracketDisplay.NodeHeader(childProps))
 				:node(BracketDisplay.NodeBody(childProps))
@@ -609,11 +606,11 @@ function BracketDisplay.NodeLowerConnectors(props)
 	local match = props.matchesById[props.matchId]
 	local layout = props.layoutsByMatchId[props.matchId]
 	local config = props.config
-	local lowerMatches = match.bracketData.lowerMatches
+	local bracketData = match.bracketData
 
 	local lowerLayouts = Array.map(
-		lowerMatches,
-		function(lowerMatch) return props.layoutsByMatchId[lowerMatch.matchId] end
+		bracketData.lowerMatchIds,
+		function(lowerMatchId) return props.layoutsByMatchId[lowerMatchId] end
 	)
 
 	-- Compute partial sums of heights of lower round matches
@@ -624,26 +621,32 @@ function BracketDisplay.NodeLowerConnectors(props)
 	-- Compute joints of connectors
 	local jointIxs = {}
 	local jointIxAbove = 0
-	for ix = math.ceil(#lowerMatches / 2), 1, -1 do
-		jointIxAbove = jointIxAbove + 1
-		jointIxs[lowerMatches[ix].opponentIx] = jointIxAbove
+	for ix = math.ceil(#bracketData.lowerEdges / 2), 1, -1 do
+		local lowerEdge = bracketData.lowerEdges[ix]
+		if not jointIxs[lowerEdge.opponentIndex] then
+			jointIxAbove = jointIxAbove + 1
+			jointIxs[lowerEdge.opponentIndex] = jointIxAbove
+		end
 	end
 	local jointIxBelow = 0
 	-- middle lower match is repeated if odd
-	for ix = math.floor(#lowerMatches / 2) + 1, #lowerMatches, 1 do
-		jointIxBelow = jointIxBelow + 1
-		jointIxs[lowerMatches[ix].opponentIx] = jointIxBelow
+	for ix = math.floor(#bracketData.lowerEdges / 2) + 1, #bracketData.lowerEdges, 1 do
+		local lowerEdge = bracketData.lowerEdges[ix]
+		if not jointIxs[lowerEdge.opponentIndex] then
+			jointIxBelow = jointIxBelow + 1
+			jointIxs[lowerEdge.opponentIndex] = jointIxBelow
+		end
 	end
 	local jointCount = math.max(jointIxAbove, jointIxBelow)
 
 	local lowerConnectorsNode = mw.html.create('div'):addClass('brkts-round-lower-connectors')
 
 	-- Draw connectors between lower round matches and this match
-	for ix, x in ipairs(lowerMatches) do
-		local lowerLayout = lowerLayouts[ix]
-		local leftTop = layout.lowerNodeMarginTop + heightSums[ix] + lowerLayout.mid
-		local rightTop = layout.matchMarginTop + ((x.opponentIx - 1) + 0.5) * config.opponentHeight
-		local jointLeft = (config.roundHorizontalMargin - 2) * jointIxs[x.opponentIx] / (jointCount + 1)
+	for _, lowerEdge in ipairs(bracketData.lowerEdges) do
+		local lowerLayout = lowerLayouts[lowerEdge.lowerMatchIndex]
+		local leftTop = layout.lowerNodeMarginTop + heightSums[lowerEdge.lowerMatchIndex] + lowerLayout.mid
+		local rightTop = layout.matchMarginTop + ((lowerEdge.opponentIndex - 1) + 0.5) * config.opponentHeight
+		local jointLeft = (config.roundHorizontalMargin - 2) * jointIxs[lowerEdge.opponentIndex] / (jointCount + 1)
 
 		local segment1Node = html.create('div'):addClass('brkts-line')
 			:css('height', config.lineWidth .. 'px')

components/match2/commons/match_group_util.lua
@@ -29,9 +29,9 @@ Display related functions go in Module:MatchGroup/Display/Helper.
 ]]
 local MatchGroupUtil = {types = {}}
 
-MatchGroupUtil.types.LowerMatch = TypeUtil.struct({
-	matchId = 'string',
-	opponentIx = 'number',
+MatchGroupUtil.types.LowerEdge = TypeUtil.struct({
+	lowerMatchIndex = 'number',
+	opponentIndex = 'number',
 })
 MatchGroupUtil.types.AdvanceBg = TypeUtil.literalUnion('up', 'stayup', 'stay', 'staydown', 'down')
 MatchGroupUtil.types.AdvanceSpot = TypeUtil.struct({
@@ -44,7 +44,8 @@ MatchGroupUtil.types.BracketBracketData = TypeUtil.struct({
 	bracketResetMatchId = 'string?',
 	bracketSection = 'string',
 	header = 'string?',
-	lowerMatches = TypeUtil.array(MatchGroupUtil.types.LowerMatch),
+	lowerMatchIds = TypeUtil.array('string'),
+	lowerEdges = TypeUtil.array(MatchGroupUtil.types.LowerEdge),
 	qualLose = 'boolean?',
 	qualLoseLiteral = 'string?',
 	qualSkip = 'number?',
@@ -273,18 +274,21 @@ end
 
 function MatchGroupUtil.bracketDataFromRecord(data, opponentCount)
 	if data.type == 'bracket' then
-		local lowerMatches = {}
-		local midIx = math.ceil(opponentCount / 2)
+		local lowerEdges = {}
+		local lowerMatchIds = {}
+		local midIx = math.floor(opponentCount / 2)
 		if nilIfEmpty(data.toupper) then
-			table.insert(lowerMatches, {
-				matchId = data.toupper,
-				opponentIx = midIx,
+			table.insert(lowerMatchIds, data.toupper)
+			table.insert(lowerEdges, {
+				opponentIndex = midIx,
+				lowerMatchIndex = #lowerMatchIds,
 			})
 		end
 		if nilIfEmpty(data.tolower) then
-			table.insert(lowerMatches, {
-				matchId = data.tolower,
-				opponentIx = math.min(midIx + 1, opponentCount),
+			table.insert(lowerMatchIds, data.tolower)
+			table.insert(lowerEdges, {
+				opponentIndex = math.min(midIx + 1, opponentCount),
+				lowerMatchIndex = #lowerMatchIds,
 			})
 		end
 
@@ -307,7 +311,8 @@ function MatchGroupUtil.bracketDataFromRecord(data, opponentCount)
 			bracketResetMatchId = nilIfEmpty(data.bracketreset),
 			bracketSection = data.bracketsection,
 			header = nilIfEmpty(data.header),
-			lowerMatches = lowerMatches,
+			lowerEdges = lowerEdges,
+			lowerMatchIds = lowerMatchIds,
 			qualLose = advanceSpots[2] and advanceSpots[2].type == 'qualify',
 			qualLoseLiteral = nilIfEmpty(data.qualloseLiteral),
 			qualSkip = tonumber(data.qualskip) or data.qualskip == 'true' and 1 or 0,
@@ -392,8 +397,8 @@ function MatchGroupUtil.computeUpperMatchIds(matchesById)
 	local upperMatchIds = {}
 	for matchId, match in pairs(matchesById) do
 		if match.bracketData.type == 'bracket' then
-			for _, lowerMatch in ipairs(match.bracketData.lowerMatches) do
-				upperMatchIds[lowerMatch.matchId] = matchId
+			for _, lowerMatchId in ipairs(match.bracketData.lowerMatchIds) do
+				upperMatchIds[lowerMatchId] = matchId
 			end
 		end
 	end
@@ -418,10 +423,7 @@ end
 function MatchGroupUtil.dfsFrom(bracketDatasById, start)
 	return TreeUtil.dfs(
 		function(matchId)
-			return Array.map(
-				bracketDatasById[matchId].lowerMatches,
-				function(lowerMatch) return lowerMatch.matchId end
-			)
+			return bracketDatasById[matchId].lowerMatchIds
 		end,
 		start
 	)
@@ -434,8 +436,8 @@ function MatchGroupUtil.computeDepthsFrom(bracketDatasById, startMatchId)
 		local bracketData = bracketDatasById[matchId]
 		depths[matchId] = depth
 		maxDepth = math.max(maxDepth, depth + bracketData.skipRound)
-		for _, lowerMatch in ipairs(bracketData.lowerMatches) do
-			visit(lowerMatch.matchId, depth + 1 + bracketData.skipRound)
+		for _, lowerMatchId in ipairs(bracketData.lowerMatchIds) do
+			visit(lowerMatchId, depth + 1 + bracketData.skipRound)
 		end
 	end
 	visit(startMatchId, 0)
@@ -497,8 +499,8 @@ function MatchGroupUtil.populateAdvanceSpots(bracket)
 	local firstBracketData = bracket.bracketDatasById[bracket.rootMatchIds[1]]
 	local thirdPlaceMatchId = firstBracketData.thirdPlaceMatchId
 	if thirdPlaceMatchId and bracket.matchesById[thirdPlaceMatchId] then
-		for _, lowerMatch in ipairs(firstBracketData.lowerMatches) do
-			local bracketData = bracket.bracketDatasById[lowerMatch.matchId]
+		for _, lowerMatchId in ipairs(firstBracketData.lowerMatchIds) do
+			local bracketData = bracket.bracketDatasById[lowerMatchId]
 			bracketData.advanceSpots[2] = bracketData.advanceSpots[2]
 				or {bg = 'stayup', type = 'advance', matchId = thirdPlaceMatchId}
 		end